a599adeec5c7e5efcefd447f8af5740ef4a8caef
Chetan Sarva <chetan@pixelcop.net>
2012-01-03 18:18:03 -0500

    fixed so it actually runs

--- src/main/java/net/pixelcop/monstrous/load/Main.java
+++ src/main/java/net/pixelcop/monstrous/load/Main.java
@@ -3,25 +3,23 @@ package net.pixelcop.monstrous.load;
 import net.pixelcop.monstrous.Job;
 
 public class Main {
-    
+
     public static void main(String[] args) throws Exception {
-        
+
+        if (args.length < 3) {
+            System.out.println("usage: test.sh <thread count> <test length in sec> URL");
+            System.exit(1);
+        }
+
         Job job = new Job();
-        job.setNumThreads(getThreadCount(args));
-        
+
+        job.setType(Job.T_TIME_LIMIT);
+        job.setNumThreads(Integer.parseInt(args[0]));
+        job.setNumSeconds(Integer.parseInt(args[1]));
+        job.setUrl(args[2]);
+
         LoadTester tester = new LoadTester(job);
         tester.start();
-    }
-    
-    public static int getThreadCount(String[] args) {
-        
-        if (args.length == 0) {
-            System.out.println("usage: test.sh <thread count>");
-            System.exit(0);
-        }
-        
-        return Integer.parseInt(args[0]);
-        
     }
 
 }
