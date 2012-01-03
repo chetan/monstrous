9eb3f9ac7031b7101f6cdb03fc74128b140f6293
Chetan Sarva <chetan@pixelcop.net>
2012-01-03 18:18:16 -0500

    write report to console on error

--- src/main/java/net/pixelcop/monstrous/load/TestWatcher.java
+++ src/main/java/net/pixelcop/monstrous/load/TestWatcher.java
@@ -1,34 +1,42 @@
 package net.pixelcop.monstrous.load;
 
+import net.pixelcop.monstrous.Stats;
 import net.pixelcop.monstrous.agent.AgentService;
 
 public class TestWatcher extends Thread {
-    
-    private LoadTester tester;
-    
+
+    private final LoadTester tester;
+
     public TestWatcher(LoadTester tester) {
         this.tester = tester;
     }
-    
+
     @Override
     public void run() {
-        
+
         System.out.println("agent: TestWatcher started");
-        
+
         long cutoff = tester.getCutoffTime();
-        
+
         while (!isInterrupted()) {
-            
+
             if (System.currentTimeMillis() >= cutoff) {
-                
+
+                Stats stats = tester.collectStats();
+
                 System.out.println("agent: going to report stats to server");
-                AgentService.getInstance().report(tester.collectStats());
+                try {
+                    AgentService.getInstance().report(stats);
+                } catch (Throwable t) {
+                    System.out.println("agent: error reproting stats to server: " + t.getMessage());
+                    stats.print(0);
+                }
                 return;
-                
+
             }
-            
+
         }
-        
+
     }
 
 }
